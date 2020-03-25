8db55710214d91514508b4f9eb141c4190d2e6c0
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-03-25 10:01:27 -0400

    notDone

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+// This is my helper function.
+ const triple = (num) => {
+  return num * 3;
+}
 
+const tripleAll = (nums) => {
+  return nums.map(triple);
+}
 
+const getNegativeValue = (number) => {
+   return Math.abs(number) * -1 + Math.sign(0); //It returns 1 if the argument passed is a positive number.
+  // It returns -1 if the argument passed is a negative number.
+  // It returns 0 if the argument passed is a positive zero.
+  // It returns -0 if the argument passed is a negative zero.
+  // If none of the above cases match,it returns Nan.
+}
 
+const negativeValues = (numbers) => {
+return numbers.map(getNegativeValue);
+}
+
+const makeQuestion = (str) => {
+return str += '?';
+}
+
+const questionings = (string) => {
+  return string.map(makeQuestion);
+}
+
+const getInitials = (initials) => {
+return initials.charAt(0) 
+}
+
+const doubleIfEven = (thisNum) => {
+  if (thisNum % 2 === 0){
+    return thisNum * 2
+  } else {
+    return thisNum
+  }
+}
+
+const doubleEven = (thoseNums) => {
+  return thoseNums.map(doubleIfEven);
+};
 
 /********************************
  * OUR CODE HERE - DON'T TOUCH! *
